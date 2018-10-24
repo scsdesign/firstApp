47413b80fc47dc5be51254fd6a64a7129a36e63f
Scott Sanders <ssanders@infoarmor.com>
2018-10-24 08:58:41 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Learn to code React</h1>
+    <p>This a totally new sentence </p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2 and some </Link>
+  </Layout>
+)
+
+export default IndexPage
+
+
